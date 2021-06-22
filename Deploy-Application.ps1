9e1bc86060e08143286c3a18edf4672ebc283dcf
jhardy6 <jhardy6@msudenver.edu>
2021-06-22 15:08:47 -0600

    -CloseApps cannot be null

--- Deploy-Application.ps1
+++ Deploy-Application.ps1
@@ -162,7 +162,7 @@ Try {
 		[string]$installPhase = 'Pre-Uninstallation'
 
 		## Show Welcome Message, close Internet Explorer with a 60 second countdown before automatically closing
-		Show-InstallationWelcome -CloseApps '' -CloseAppsCountdown 60
+		Show-InstallationWelcome -CloseApps 'iexplore' -CloseAppsCountdown 60
 
 		## Show Progress Message (with the default message)
 		Show-InstallationProgress
